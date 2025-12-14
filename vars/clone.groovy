a4715aafbd1e6f4c5e0aeba17ae07c21bb28a3dc
devops_HASNAIN <techtostory@gmail.com>
2025-12-14 16:12:54 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,6 +1,6 @@
 def call(String url,String branch){
    echo "hello world thid is clonig"
-   git url: url,branch: branch
+   git url: "${url}",branch: "${branch}"
    echo "cloning successfull"
 
   
